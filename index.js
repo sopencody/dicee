2e653ff207df315d3c31f3e5f7f5bcaa0578db5c
sopennn <58549604+sopennn@users.noreply.github.com>
2022-05-30 14:28:27 +0700

    third commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var randomImgSrc = "images/" + randomDiceImg;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;//1-6
+
+var randomDiceImg = "dice" + randomNumber1 + "png";//dice1.png - dice6.png
+
+var randomImgSrc = "images/dice" + randomDiceImg;//images/dice1.png - images/dice6.png
 
 var img1 = document.querySelectorAll("img")[0];
 
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
-var randomDiceImg = "dice" + randomNumber1 + "png";
 
 img1.setAttribute("src", randomImgSrc);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSources2 = "images/" + randomNumber2 + "png";
+var randomImageSources2 = "images/dice" + randomNumber2 + "png";
 
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSources2);
 
